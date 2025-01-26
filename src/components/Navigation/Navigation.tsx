55d999ba666d2751b9314b4457fc95f25bbfcf21
shuma sajimoto <saji@shumanoMacBook-Air.local>
2025-01-27 03:44:55 +0900

    feat: 大枠のページ作った

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import styles from "./Navigation.module.css";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const pathname = usePathname();
+
+  return (
+    <nav className={styles.nav}>
+      <a
+        href="/game"
+        className={`${styles.tab} ${pathname === "/game" ? styles.active : ""}`}
+      >
+        対局
+      </a>
+      <a
+        href="/user"
+        className={`${styles.tab} ${pathname === "/user" ? styles.active : ""}`}
+      >
+        ユーザー
+      </a>
+      <a
+        href="/score"
+        className={`${styles.tab} ${pathname === "/score" ? styles.active : ""}`}
+      >
+        成績
+      </a>
+      <a
+        href="/ranking"
+        className={`${styles.tab} ${pathname === "/ranking" ? styles.active : ""}`}
+      >
+        ランキング
+      </a>
+    </nav>
+  );
+}
